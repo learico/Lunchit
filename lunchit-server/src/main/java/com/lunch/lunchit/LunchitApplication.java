4b721fd3fd5ea1742cdee8235b2d85ecd64d859e
Limelette Léa <lealimelette@yahoo.fr>
2020-01-17 10:34:39 +0100

    init server

---
+++ lunchit-server/src/main/java/com/lunch/lunchit/LunchitApplication.java
@@ -0,0 +1,13 @@
+package com.lunch.lunchit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LunchitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LunchitApplication.class, args);
+	}
+
+}
